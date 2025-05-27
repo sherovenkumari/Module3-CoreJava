04e9b29fbba2391c7256c415eaef6b797f202757
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-27 11:07:46 +0530

    Add files via upload

---
+++ BytecodeExample.java
@@ -0,0 +1,10 @@
+
+public class BytecodeExample {
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
+
+// Compile and run:
+// javac BytecodeExample.java
+// javap -c BytecodeExample
