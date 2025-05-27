981fe0f5fd799b80ee955a0641df6975079ded6a
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-27 11:09:17 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 3.14f;
+        double d = 9.81;
+        char c = 'A';
+        boolean b = true;
+
+        System.out.println("int: " + i);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + b);
+    }
+}
