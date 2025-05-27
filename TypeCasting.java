082fb6ad6990478b0ab88d6ebf65af7b2328ce8c
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-27 11:18:36 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        double d = 9.99;
+        int i = (int) d;
+        System.out.println("Double to int: " + i);
+
+        int x = 10;
+        double y = x;
+        System.out.println("Int to double: " + y);
+    }
+}
