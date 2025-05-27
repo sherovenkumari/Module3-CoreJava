7cea52c2ac679b4c03e4236db166430dbc2aca34
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-27 11:11:49 +0530

    Add files via upload

---
+++ JdbcConnect.java
@@ -0,0 +1,20 @@
+
+import java.sql.*;
+
+public class JdbcConnect {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:students.db");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + " " + rs.getString("name"));
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Database error: " + e.getMessage());
+        }
+    }
+}
